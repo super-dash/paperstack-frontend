3d02bb89450982d21388ab310b2b03d90530ff9e
dangosky <1341514988@qq.com>
2019-09-22 19:38:33 +0800

    feat: home-page
    Change-Id: I046eef43709c55677f7c314f9d2e07c361fc3aed

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+document.title = '超自然作业派';
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import './style.less';
+import backgroundPng from '../../images/home-background.png';
+import gzhuLogoPng from '../../images/gzhu-logo.png';
 
 class Home extends Component {
   render() {
     return (
-      <div className="p-home"></div>
+      <div className="p-home">
+        <div className="home-container">
+          <img src={gzhuLogoPng} alt="background.png"/>
+          <div className="title-box">
+            <h1>超自然作业派</h1>
+            <h1>Paper Stack</h1>
+            <h2>博学笃行，与时俱进</h2>
+          </div>
+          <div className="button-box">
+            <button>登陆</button>
+            <button>注册</button>
+          </div>
+        </div>
+      </div>
     )
   }
 }
