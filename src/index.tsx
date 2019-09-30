a721d0a7b28c4b9d6af0f773661739eb35d72e2c
dangosky <1341514988@qq.com>
2019-09-30 14:45:43 +0800

    style: 适配页面
    Change-Id: I117b0d965b8ce83891e869eb067eff07b0da2df0

--- src/index.tsx
+++ src/index.tsx
@@ -5,4 +5,13 @@ import 'antd/dist/antd.css';
 
 document.title = '超自然作业派';
 
+(function judgeMobile() {
+  const ua = window.navigator.userAgent.toLowerCase();
+  const isAndroid = ua.indexOf('android') > -1 || ua.indexOf('adr') > -1;
+  const isIOS = !!ua.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/i);
+  if (isAndroid || isIOS) {
+    document.body.classList.add('mobile');
+  }
+})();
+
 ReactDOM.render(<App baseUrl={window.location.pathname}/>, document.getElementById('root'));
