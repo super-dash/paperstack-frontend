5524e2e55f5911547c4ad063dbe9186797721670
dangosky <1341514988@qq.com>
2019-11-08 00:58:14 +0800

    feat: finish user profile page UI
    Change-Id: I28d76692875b8b65d5f5a9d03cd5038ab4387cca

--- src/pages/user/profile/index.tsx
+++ src/pages/user/profile/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import './style.less';
 import { Button } from 'antd';
 import { autobind } from 'core-decorators';
 import EditProfile from '@src/pages/user/edit';
@@ -12,7 +13,7 @@ class UserProfile extends Component<{}, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      modalVisible: true
+      modalVisible: false
     };
   }
 
@@ -33,7 +34,42 @@ class UserProfile extends Component<{}, State> {
 
     return (
       <Fragment>
-        <Button type="primary" onClick={this.showModal}>编辑个人信息</Button>
+        <div className="profile-container">
+          <div className="porfile-box">
+            <div className="portrait">用户头像</div>
+            <div className="user-info">
+              <p>有马公生</p>
+              <p>
+                <span>性别：</span>
+                <span>学院：</span>
+              </p>
+              <p>
+                <span>学号：</span>
+                <span>班级：</span>
+              </p>
+            </div>
+            <div className="edit-info-btn">
+              <Button type="primary" onClick={this.showModal}>编辑个人信息</Button>
+            </div>
+          </div>
+          <div className="user-info-container">
+            <div className="user-projects">
+              <div className="project-setting">
+                <label>我参与的项目</label>
+                <span>新建作业项目</span>
+                <span>导出</span>
+              </div>
+              <div className="project-content"></div>
+            </div>
+            <div className="user-setting">
+              <span>账号绑定</span>
+              <span>修改邮箱地址</span>
+              <span>修改密码</span>
+              <span>注销账号</span>
+              <span>个性化设置</span>
+            </div>
+          </div>
+        </div>
         <EditProfile
           modalVisible={modalVisible}
           handleCancel={this.handleCancel}
