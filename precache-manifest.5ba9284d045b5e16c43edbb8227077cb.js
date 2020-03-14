6d55056cccf80a910df36af7aae8ef75e73089ea
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-14 05:48:58 +0000

    Deploy super-dash/paperstack-frontend to github.com/super-dash/paperstack-frontend.git:gh-pages

---
+++ precache-manifest.5ba9284d045b5e16c43edbb8227077cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1070c507098c5e0327658a0df523f0",
+    "url": "https://super-dash.github.io/paperstack-frontend/index.html"
+  },
+  {
+    "revision": "57604ddf9e0dc196cf0b",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/css/2.cc82b09a.chunk.css"
+  },
+  {
+    "revision": "1457d48e6b3c1fed0d67",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/css/main.e6e929ed.chunk.css"
+  },
+  {
+    "revision": "57604ddf9e0dc196cf0b",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/js/2.ebecdae3.chunk.js"
+  },
+  {
+    "revision": "1457d48e6b3c1fed0d67",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/js/main.0d26beaf.chunk.js"
+  },
+  {
+    "revision": "dc8954e772a272422dd5",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/js/runtime-main.c9c5b668.js"
+  },
+  {
+    "revision": "8b24e9b7d357e648529e0dcf9d6fbe90",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/media/gzhu-logo.8b24e9b7.png"
+  },
+  {
+    "revision": "403819d64331a96bb68b1757c70ebdbe",
+    "url": "https://super-dash.github.io/paperstack-frontend/static/media/home-background.403819d6.jpg"
+  }
+]);
